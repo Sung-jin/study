7e352989b5b0e8aba45168fa7950a32f70b8ad42
SungJin <osj4872@gmail.com>
2019-10-22 23:34:23 +0900

    * 테스트 내용 추가_2

--- spring/code/src/main/java/DAO/UserDao.java
+++ spring/code/src/main/java/DAO/UserDao.java
@@ -1,6 +1,7 @@
 package DAO;
 
 import DTO.User;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import javax.sql.DataSource;
 import java.sql.*;
@@ -12,7 +13,7 @@ public class UserDao {
         this.dataSource = dataSource;
     }
 
-    public void add(User user) throws SQLException {
+    void add(User user) throws SQLException {
         Connection c = dataSource.getConnection();
 
         PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
@@ -27,7 +28,7 @@ public class UserDao {
         c.close();
     }
 
-    public User get(String id) throws ClassNotFoundException, SQLException {
+    User get(String id) throws SQLException {
 
         Connection c = dataSource.getConnection();
 
@@ -36,17 +37,49 @@ public class UserDao {
         ps.setString(1, id);
 
         ResultSet rs = ps.executeQuery();
-        rs.next();
 
-        User user = new User();
-        user.setId(rs.getString("id"));
-        user.setName(rs.getString("name"));
-        user.setPassword(rs.getString("password"));
+        User user = null;
+        if(rs.next()){
+            user = new User();
+            user.setId(rs.getString("id"));
+            user.setName(rs.getString("name"));
+            user.setPassword(rs.getString("password"));
+        }
 
         rs.close();
         ps.close();
         c.close();
 
+        if (user == null) throw new EmptyResultDataAccessException(1);
+
         return user;
+    }
+
+    void deleteAll() throws SQLException {
+        Connection c = dataSource.getConnection();
+
+        PreparedStatement ps = c.prepareStatement("delete from users");
+
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+    }
+
+    int getCount() throws SQLException {
+
+        Connection c = dataSource.getConnection();
+
+        PreparedStatement ps = c.prepareStatement("select count(*) from users");
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        int count = rs.getInt(1);
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return count;
     }
 }
--- spring/code/src/main/java/main.java
+++ spring/code/src/main/java/main.java
@@ -1,27 +1,7 @@
-import DAO.UserDao;
-import DTO.User;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
-
-import java.sql.SQLException;
+import org.junit.runner.JUnitCore;
 
 public class main {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-
-        UserDao aDAO = context.getBean("userDao", UserDao.class);
-
-        User aUser = new User();
-        aUser.setId("fonnie");
-        aUser.setName("오성진");
-        aUser.setPassword("1234");
-
-        aDAO.add(aUser);
-
-        System.out.println("a 설정 DB에 등록성공");
-
-        User aUser2 = aDAO.get(aUser.getId());
-        System.out.println(aUser2.getName());
-        System.out.println(aUser2.getPassword());
+    public static void main(String[] args) {
+        JUnitCore.main("DAO.UserDaoTest");
     }
 }
