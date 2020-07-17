cb5569098a435fa33b202942addcc6cbf7f62e97
osj <sj.oh@anypointmedia.com>
2020-07-17 18:48:15 +0900

    카카오 괄호 문제 중간 커밋

--- alhorithm/kotlin/src/Main.kt
+++ alhorithm/kotlin/src/Main.kt
@@ -1,8 +1,14 @@
-fun main() {
-    val kakao2020 = KAKAO2020()
-    println(kakao2020.compression("aabbaccc").length)
-    println(kakao2020.compression("ababcdcdababcdcd").length)
-    println(kakao2020.compression("abcabcdede").length)
-    println(kakao2020.compression("abcabcabcabcdededededede").length)
-    println(kakao2020.compression("xababcdcdababcdcd").length)
+fun main(args: Array<String>) {
+//    val kakao2020Compression = KAKAO2020COMPRESSION()
+    val kakao2020ParenthesesConversion = KAKAO2020ParenthesesConversion()
+//
+    kakao2020ParenthesesConversion.parenthesesConversion("(()())()")
+    kakao2020ParenthesesConversion.parenthesesConversion(")(")
+    kakao2020ParenthesesConversion.parenthesesConversion("()))((()")
+
+//    println(kakao2020Compression.compression("aabbaccc").length)
+//    println(kakao2020Compression.compression("ababcdcdababcdcd").length)
+//    println(kakao2020Compression.compression("abcabcdede").length)
+//    println(kakao2020Compression.compression("abcabcabcabcdededededede").length)
+//    println(kakao2020Compression.compression("xababcdcdababcdcd").length)
 }
