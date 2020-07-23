de1394def0ac20bac5957da39b72e0045986c434
osj <sj.oh@anypointmedia.com>
2020-07-23 20:16:47 +0900

    vue nuxt ts vuetify init

---
+++ spring/project/FE/pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card>
+    <v-card-title>hi</v-card-title>
+    <logo></logo>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+import Logo from '~/components/Logo.vue'
+
+@Component({
+  components: {
+    Logo
+  }
+})
+export default class extends Vue {
+
+}
+</script>
+
+<style>
+</style>
