e33657c50e0d0b8931cd596904dd608007288818
Sung-jin <osj4872@gmail.com>
2021-03-13 21:56:09 +0900

    react 를 이용하여 틱택톡 게임 구현

--- react/my-app/README.md
+++ react/my-app/README.md
@@ -33,4 +33,27 @@ return React.createElement('div', {className: 'some-class'}, this.props.someValu
 
 * JSX 내부의 중괄호 안에 어떤 javascript 표현식도 사용할 수 있다.
 * React 엘리먼트는 JavaScript 객체이며 변수에 저장하거나 프로그램 여기저기에 전달할 수 있다.
-* 
+ 
+#### 불변성
+
+* 원본 객체를 직접 수정하지 않고, 복사본을 만들어서 수정하고 적용할 때 불변성이 적용된다.
+
+```javascript
+const originalArray = [1,2,3,4,5];
+
+...
+
+const copyArray = originalArray.splice();
+copyArray[1] = 20;
+originalArray = copyArray;
+```
+
+* 원본을 직접 수정하지 않고 카피본을 만들어서 불변성을 이용하는 이유
+    1. 복잡한 특징들을 단순하게 만듦
+        * undo 기능을 구현하기에는 직접 원본을 수정하는 것 보다는 수정본을 이용해서 수정하는 것이 더 편하다.
+    2. 변화를 감지함
+        * 객체를 직접 수정하는 경우에는 복제가 가능한 객체에서 변화를 감지하는 것은 어렵다.
+    3. react 에서 다시 렌더링하는 시기를 결정함
+        * react 에서 다시 렌더링하는 시기를 결정하는데, 이는 react 에서 순수 컴포넌트를 만드는데 도움을 준다.
+        * 변하지 않는 데이터는 변경이 이루어졌는지 쉽게 판단할 수 있고, 이를 바탕으로 컴포넌트가 다시 렌더링할지를 결정할 수 있다.
+* [성능 최적화하기](https://ko.reactjs.org/docs/optimizing-performance.html#examples)
--- react/my-app/src/index.js
+++ react/my-app/src/index.js
@@ -2,42 +2,88 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        // javascript 클래스에서 하위 클래스의 생성자를 정의할 떄 항상 super 를 호출해야 한다.
-        // 모든 react 컴포넌트 클래스는 생성자를 가질 때 super(props) 호출 구문부터 작성해야 한다.
-        this.state = {
-            value: null,
-        };
-    }
-    // 해당 컴포넌트의 어떠한 값을 기억할 때 state 를 사용한다.
-    render() {
-        return (
-            // <button className="square" onClick={() => alert('click')}>
-            <button
-                className="square"
-                onClick={() => this.setState({value: 'X'})}
-            >
-                {/*state 의 값을 변경할 때 this.setState 를 사용한다.*/}
-                {/*setState 를 호출하면 React 는 자동으로 컴포넌트 내부의 자식 컴포넌트 역시 업데이트를 진행한다.*/}
-                {/*{this.props.value}*/}
-                {this.state.value}
-            </button>
-        );
-    }
-    // 부모에게 전달받은 props 의 value 를 사용한다.
+// class Square extends React.Component {
+//     // constructor(props) {
+//     //     super(props);
+//     //     // javascript 클래스에서 하위 클래스의 생성자를 정의할 떄 항상 super 를 호출해야 한다.
+//     //     // 모든 react 컴포넌트 클래스는 생성자를 가질 때 super(props) 호출 구문부터 작성해야 한다.
+//     //     this.state = {
+//     //         value: null,
+//     //     };
+//     // }
+//     // 해당 컴포넌트의 어떠한 값을 기억할 때 state 를 사용한다.
+//
+//     // 게임의 상태를 square 의 부모인 board 에서 관리하기 때문에 해당 컴포넌트에서 상태를 관리할 필요가 없어진다.
+//     // 이러한 컴포넌트를 Board 컴포넌트에 '제어되는 컴포넌트' 라고 한다.
+//     render() {
+//         return (
+//             // <button className="square" onClick={() => alert('click')}>
+//             <button
+//                 className="square"
+//                 onClick={() => this.props.onClick()}
+//             >
+//                 {/*state 의 값을 변경할 때 this.setState 를 사용한다.*/}
+//                 {/*setState 를 호출하면 React 는 자동으로 컴포넌트 내부의 자식 컴포넌트 역시 업데이트를 진행한다.*/}
+//                 {this.props.value}
+//                 {/*{this.state.value}*/}
+//             </button>
+//         );
+//     }
+//     // 부모에게 전달받은 props 의 value 를 사용한다.
+// }
+// // 스퀘어 컴포넌트
+
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {/*this.props -> props 로 변경됨.*/}
+            {props.value}
+        </button>
+    );
 }
-// 스퀘어 컴포넌트
+// state 가 없이 컴포넌트를 작성할 때 함수 컴포넌트로 작성할 수 있다.
+// 함수 컴포넌트는 props 를 전달받아서 렌더링할 대상을 반환하는 함수이다.
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true,
+        };
+    }
+
     renderSquare(i) {
-        return <Square value={i} />;
+        return <Square
+            value={this.state.squares[i]}
+            onClick={() => this.handleClick(i)}
+        />;
     }
     // 부모인 board 가 자식 컴포넌트인 square 에 value 라는 props 를 전달
 
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+            // 이미 클릭된 곳일 경우 해당 이벤트는 무시
+        }
+        // .slice 를 통해 기존 배열을 수정하지 않고 복사본을 생성해서 수정을 진행했다.
+        // 이는 불변성을 지키기 위해서이다.
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
+    }
+
     render() {
-        const status = 'Next player: X';
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = `Winner: ${winner}`;
+        } else {
+            status = `Next player : ${this.state.xIsNext ? 'X' : 'O'}`;
+        }
 
         return (
             <div>
@@ -62,6 +108,26 @@ class Board extends React.Component {
     }
 }
 // 보드 컴포넌트
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
 
 class Game extends React.Component {
     render() {
