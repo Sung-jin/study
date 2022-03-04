36297ba63f1bbe726fc1a855f984d2c3d1a76855
Sung-jin <osj4872@gmail.com>
2022-03-04 22:55:37 +0900

    * 빈 생명주기 콜백

---
+++ spring/inflearn/UnderstandingSpringCorePrinciple/src/test/java/com/example/understandingspringcoreprinciple/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,28 @@
+package com.example.understandingspringcoreprinciple.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void liefCycleTest() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+//        @Bean(initMethod = "init", destroyMethod = "close")
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hell-spring.dev");
+            return networkClient;
+        }
+    }
+}
