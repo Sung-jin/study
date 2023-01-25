4558507ca580cf966a4532fb325793e7543b8cb4
Sung-jin <osj4872@gmail.com>
2023-01-25 22:19:04 +0900

    프록시 패턴

--- spring/toby's spring/toby/dao/src/service/UserServiceTx.java
+++ spring/toby's spring/toby/dao/src/service/UserServiceTx.java
@@ -3,7 +3,7 @@ package service;
 import domain.User;
 
 public class UserServiceTx implements UserService {
-    UserService userService;
+    UserService userService; // 타깃 오브젝트
     PlatformTransactionManager transactionManager;
 
     public void setUserService(UserService userService) {
@@ -16,18 +16,21 @@ public class UserServiceTx implements UserService {
 
     @Override
     public void add(User user) {
+        // 메소드 구현과 위임
         userService.add(user);
     }
 
     @Override
     public void upgradeLevels() {
+        // 메소드 구현
         TransactionStatus status = this.transactionManager.getTransaction(new DefaultTransactionDefinition());
+        // 부가기능
 
         try {
-            userService.upgradeLevels();
-            this.transactionManager.commit(status);
+            userService.upgradeLevels(); // 위임
+            this.transactionManager.commit(status); // 부가기능
         } catch (RuntimeException e) {
-            this.transactionManager.rollback(sstatus);
+            this.transactionManager.rollback(sstatus); // 부가기능
             throw e;
         }
     }
