6c0d16ff3fc9424a0c583749b1218214b2084c90
Sung-jin <osj4872@gmail.com>
2023-01-30 23:59:22 +0900

    * 프록시 팩토리 빈 장점

--- spring/toby's spring/toby/dao/src/service/TxProxyFactoryBean.java
+++ spring/toby's spring/toby/dao/src/service/TxProxyFactoryBean.java
@@ -5,6 +5,14 @@ import proxy.TransactionHandler;
 
 import java.lang.reflect.Proxy;
 
+/**
+ * 프록시를 적용할 경우, 적용할 대상이 구현하고 있는 인터페이스를 구현하는 프록시 클래스를 다 구현해야 하며,
+ * 부가적인 기능이 여러 메소드에 반복적으로 나타나는 문제점이 존재한다
+ *
+ * 프록시 빈 팩토리는 이러한 문제점을 모두 해결해 준다
+ * - 다이내믹 프록시를 이용하면 타깃 인터페이스를 모두 구현하는 작업을 제거할 수 있다
+ * - 하나의 핸들러 메소드를 구현하여 많은 메소드에 부가기능을 제공할 수 있으므로, 중복 문제도 해결된다
+ */
 public class TxProxyFactoryBean implements FactoryBean<Object> {
     Object target;
     PlatformTransactionManager transactionManager;
