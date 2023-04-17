0ba9acfdc767417426cca974b36adbacea063624
Sung-jin <osj4872@gmail.com>
2023-04-17 23:45:17 +0900

    위임을 이용한 baseService 활용

--- spring/toby's spring/toby/dao/src/sql/OxmSqlService.java
+++ spring/toby's spring/toby/dao/src/sql/OxmSqlService.java
@@ -9,6 +9,8 @@ import javax.xml.transform.stream.StreamSource;
 import java.io.IOException;
 
 public class OxmSqlService implements SqlService {
+    private final BaseSqlService baseSqlService = new BaseSqlService();
+
     private final OxmSqlReader oxmSqlReader = new OxmSqlReader();
     private SqlRegistry sqlRegistry = new HashMapSqlRegistry();
 
@@ -25,17 +27,16 @@ public class OxmSqlService implements SqlService {
     }
 
     @PostConstructor
-    public void initSql() {
-        this.oxmSqlReader.read(this.sqlRegistry);
+    public void loadSql() {
+        this.baseSqlService.setSqlReader(this.oxmSqlReader);
+        this.baseSqlService.setSqlRegistry(this.sqlRegistry);
+
+        this.baseSqlService.loadSql();
     }
 
     @Override
     public String getSql(String key) throws SqlRetrievalFailureException {
-        try {
-            return this.sqlRegistry.findSql(key);
-        } catch (SqlNotFoundException e) {
-            throw new SqlRetrievalFailureException(e);
-        }
+        return this.baseSqlService.getSql(key);
     }
 
     private class OxmSqlReader implements SqlReader {
